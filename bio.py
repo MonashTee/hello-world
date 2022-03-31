babd7441d24785feb3d68051fc3e61eec6652f53
MonashTee <91289484+MonashTee@users.noreply.github.com>
2022-03-31 18:32:32 +0200

    Update bio.py

--- bio.py
+++ bio.py
@@ -1,1 +1,6 @@
-
+# a simple code for biography
+name = ' Tehillah '
+age = ' one year '
+bio = name +  " " + one year 
+print (str(name) + ' is ' + str(age) + ' old ')
+# return result Tehillah is one year old
