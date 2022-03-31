c3ee9a9427b5f17a45875cd01277090d0cd90ba9
MonashTee <91289484+MonashTee@users.noreply.github.com>
2022-03-31 18:13:57 +0200

    Update sale_tax.py

--- sale_tax.py
+++ sale_tax.py
@@ -1,1 +1,5 @@
-print('hello")
+#writing a simple code on adding tax amount to a sale price.
+sales amount = 2000
+tax = 0.05
+sales total = sales amount + sales amount * tax
+print (sales total) # to return the gross value.
