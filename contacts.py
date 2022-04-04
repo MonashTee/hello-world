5b41ab81e0c9c433030c8a02f598a347ad92d235
MonashTee <91289484+MonashTee@users.noreply.github.com>
2022-04-04 15:28:58 +0200

    Create contacts.py

---
+++ contacts.py
@@ -0,0 +1,9 @@
+contacts = {"numbers" : 2, "workers":
+            [
+              {"name" : "Alex Bush", "email" : "alexbush@gmail.com"},
+              {"name" : "Clinton Hilton", "email" : "clinton_hilton@gmail.com"}
+            ]
+           }
+ for worker in contacts ["workers"]:
+    print (worker)
+                          
