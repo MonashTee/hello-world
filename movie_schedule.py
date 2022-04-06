e2764f0ecd9279dc366f139590d69944d3972c93
MonashTee <91289484+MonashTee@users.noreply.github.com>
2022-04-06 20:50:46 +0200

    Create movie_schedule.py

---
+++ movie_schedule.py
@@ -0,0 +1,5 @@
+current_movies = {"King of boys" : "10:00am" , "Coming to America" : "1:00pm",
+                  "Fast and Furious" : "5:00pm"}
+print ("movies now showing")
+for x in current_movies:
+  print (x)
