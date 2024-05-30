80082250c95a523db360e7cc6651e6f42f3ec678
Temitope Akinmolayan <91289484+MonashTee@users.noreply.github.com>
2024-05-30 21:58:00 +0200

    Update bio.py

--- bio.py
+++ bio.py
@@ -1,6 +1,12 @@
 # a simple code for biography
 name = ' Tehillah '
 age = ' one year '
-bio = name +  " " + one year 
+bio = name +  " " + age 
 print (str(name) + ' is ' + str(age) + ' old ')
 # return result Tehillah is one year old
+
+name = ' Elijah '
+age = ' Thirty years'
+bio = name + " " + age
+print (str(name) + ' is ' +str(age) + ' old ')
+# return result Elijah is Thirty years old
